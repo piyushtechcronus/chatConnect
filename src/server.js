20c5a011a6a70f816d58d8bd29d8780a9be48692
piyushtechcronus <Piyush.dabhi@techcronus.com>
2024-02-15 13:11:45 +0530

    username feature

--- src/server.js
+++ src/server.js
@@ -1,46 +1,34 @@
-// Require necessary modules
 const express = require('express');
 const http = require('http'); 
 const path = require('path');
 const socketio = require('socket.io');  
 
-// Create Express app
 const app = express();
 const server = http.createServer(app); 
-
-// Set up view engine
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
-// Serve static files
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Initialize Socket.IO and attach it to the server
 const io = socketio(server);
 
-// Add event listener for 'connection' event
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
 io.on('connection', (socket) => {
   console.log('A user connected');
 
-  // Event listener for 'sendMessage' event
+  let username;
+
   socket.on('sendMessage', (data) => {
-    // Emit the message to all clients except the sender
-    socket.broadcast.emit('messageReceived', data);
+    io.emit('messageReceived', data);
   });
 
-  // Event listener for 'disconnect' event
   socket.on('disconnect', () => {
     console.log('A user disconnected');
   });
 });
 
-// Define route to handle GET requests to "/chat"
 app.get('/', (req, res) => {
-  // Render the chat page using an EJS template
-  res.render('chat'); // Assuming you have a "chat.ejs" template in your views directory
+  res.render('chat');
 });
- 
-// Start the server
+
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
