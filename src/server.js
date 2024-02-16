fd1d88341a296733f2c17bba195c93374827d4e0
piyushtechcronus <Piyush.dabhi@techcronus.com>
2024-02-16 11:55:34 +0530

    text alignemt

--- src/server.js
+++ src/server.js
@@ -12,47 +12,54 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
-  console.log('A user connected');
+    console.log('A user connected');
 
-  let username;
+    let username;
 
-  socket.on('sendMessage', (data) => {
-    console.log('activity: ',data);
-    io.emit('messageReceived', data);
-    
-  });
+    socket.on('sendMessage', (data) => {
+        console.log('activity: ',data);
+        io.emit('messageReceived', data);
+    });
 
-  socket.on('disconnect', () => {
-    console.log('A user disconnected');
-  });
+    socket.on('typing', (username) => {
+        socket.broadcast.emit('userTyping', username);
+    });
+
+    socket.on('stopTyping', () => {
+        socket.broadcast.emit('userStoppedTyping');
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
 });
 
 app.get('/', (req, res) => {
-  res.render('chat');
+    res.render('chat');
 });
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  const message = `The server is running on port localhost:${PORT}`;
-  const level = 'info';
-  const timestamp = formatTimestamp(serverStartTime);
+    const message = `The server is running on port localhost:${PORT}`;
+    const level = 'info';
+    const timestamp = formatTimestamp(serverStartTime);
   
-  const logData = {
-    message,
-    level,
-    timestamp
-  };
+    const logData = {
+        message,
+        level,
+        timestamp
+    };
 
-  console.log(JSON.stringify(logData, null, 2));
+    console.log(JSON.stringify(logData, null, 2));
 });
 
 // Function to format the timestamp to YYYY-MM-DD HH:MM:SS format
 function formatTimestamp(date) {
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
-  const hours = String(date.getHours()).padStart(2, '0');
-  const minutes = String(date.getMinutes()).padStart(2, '0');
-  const seconds = String(date.getSeconds()).padStart(2, '0');
-  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 }
